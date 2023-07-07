100d8c245d648c1d06fc8b77fed274d0e0485b91
YunbaiLi <1144370566@qq.com>
2023-07-07 11:41:59 +0800

    1

---
+++ heima-leadnews-service/heima-leadnews-article/src/main/java/com/heima/article/ArticleApplication.java
@@ -0,0 +1,32 @@
+package com.heima.article;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import lombok.extern.slf4j.Slf4j;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@SpringBootApplication
+@EnableDiscoveryClient
+@MapperScan("com.heima.article.mapper")
+public class ArticleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArticleApplication.class, args);
+        log.info("启动成功");
+    }
+
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+}
