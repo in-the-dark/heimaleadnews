9dda055870e69870caac867ba5ff2d6c161016c2
YunbaiLi <1144370566@qq.com>
2023-07-10 21:15:32 +0800

    第四天代码

--- heima-leadnews-service/heima-leadnews-article/src/main/java/com/heima/article/ArticleApplication.java
+++ heima-leadnews-service/heima-leadnews-article/src/main/java/com/heima/article/ArticleApplication.java
@@ -9,12 +9,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @SpringBootApplication
 @EnableDiscoveryClient
 @MapperScan("com.heima.article.mapper")
+@EnableAsync
 public class ArticleApplication {
 
     public static void main(String[] args) {
